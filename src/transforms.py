16a9aef930b88e44196daae1ee7a37b2995e8160
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2021-11-09 23:36:28 +0530

    refactor to avoid code repetition

--- src/transforms.py
+++ src/transforms.py
@@ -11,11 +11,7 @@ def compose(funcs):
 
     return reduce(inner, funcs, lambda x: x)
 
-class Clahe(QAction):
-    def __init__(self, parent=None):
-        super().__init__('Adaptive Histogram Equalization', parent)
-        self.triggered.connect(self.setup)
-
+class ClaheBase(QAction):
     def setup(self):
         self.grid = 1
         self.clip = 1
@@ -51,6 +47,11 @@ class Clahe(QAction):
     def update_clip(self, value):
         self.clip = value*0.1
         self.win.parent().parent().show_img()
+
+class Clahe(ClaheBase):
+    def __init__(self, parent=None):
+        super().__init__('Adaptive Histogram Equalization', parent)
+        self.triggered.connect(self.setup)
 
     def __call__(self, img):
         clahe = cv2.createCLAHE(clipLimit=self.clip, tileGridSize=(self.grid, self.grid))
@@ -61,46 +62,10 @@ class Clahe(QAction):
 
         return img
 
-class FalseClahe(QAction):
+class FalseClahe(ClaheBase):
     def __init__(self, parent=None):
         super().__init__('False Adaptive Histogram Equalization', parent)
         self.triggered.connect(self.setup)
-
-    def setup(self):
-        self.grid = 1
-        self.clip = 1
-        applied.append(self)
-
-        self.win = QMainWindow(self.parent())
-        self.win.setWindowTitle(self.text())
-
-        grid_slider = QSlider(Qt.Horizontal, self.win)
-        grid_slider.setRange(1, 100)
-        grid_slider.setFocusPolicy(Qt.NoFocus)
-        grid_slider.setPageStep(5)
-        grid_slider.valueChanged.connect(self.update_grid)
-
-        clip_slider = QSlider(Qt.Horizontal, self.win)
-        clip_slider.setRange(1, 100)
-        clip_slider.setFocusPolicy(Qt.NoFocus)
-        clip_slider.setPageStep(5)
-        clip_slider.valueChanged.connect(self.update_clip)
-
-        wid = QWidget(self.win)
-        layout = QVBoxLayout()
-        layout.addWidget(clip_slider)
-        layout.addWidget(grid_slider)
-        wid.setLayout(layout)
-        self.win.setCentralWidget(wid)
-        self.win.show()
-
-    def update_grid(self, value):
-        self.grid = value
-        self.win.parent().parent().show_img()
-
-    def update_clip(self, value):
-        self.clip = value*0.1
-        self.win.parent().parent().show_img()
 
     def __call__(self, img):
         clahe = cv2.createCLAHE(clipLimit=self.clip, tileGridSize=(self.grid, self.grid))
