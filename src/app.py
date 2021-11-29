03ea0c8a2dbab8d1e8cd6fd9d6a539aa6f463ae6
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2021-11-29 15:52:32 +0530

    rename file

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import cv2
-from image import toPixmap
+from utils import toPixmap
 import transforms as t
 
 class FileMenu(QMenu):
